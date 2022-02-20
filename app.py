b6504a5e9c45e4830d9a6d0945e3a89e36ea8da0
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-02-20 13:57:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,6 @@ b = st.slider(label = 'Sepal Width',min_value = 0.50,max_value = 4.40)
 c = st.slider(label = 'Petal Length',min_value = 0.50,max_value = 6.90)
 d = st.slider(label = 'Petal Width',min_value = 0.50,max_value = 6.90)
 result = model.predict([[a,b,c,d]])
-if result == 0:
-  st.text('Setosa')
-if result == 1:
-  st.text('versicolor')
-if result ==2:
-  st.text('verginica')
+st.title(irist.target_names[result])
 
   
