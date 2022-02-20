af6dcd5e3b1db7fd1055617ea191dc56caaa8d73
Karthik G <68554363+lelouch248@users.noreply.github.com>
2022-02-20 13:41:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 #%%writefile app.py
 import streamlit as st
 import joblib
+import pandas as pd
+from sklearn import datasets
+
+iris = datasets.load_iris()
+x = iris.data
+y = iris.target
+
+df = pd.DataFrame(iris.data)
+df['Target'] = iris.target
+
+from pandas.core.common import not_none
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(x,y)
+
 st.title('IRIS CLASSIFIER - API')
 a = st.slider(label = 'Sepal Length',min_value = 0.50,max_value = 7.90)
 b = st.slider(label = 'Sepal Width',min_value = 0.50,max_value = 4.40)
